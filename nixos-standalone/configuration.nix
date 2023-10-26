466dada952c3b0f3b538c02e36052a3ac3b06844
Jens Peter Secher <jpsecher@gmail.com>
2023-10-26 19:58:32 +0200

    home-manager

--- nixos-standalone/configuration.nix
+++ nixos-standalone/configuration.nix
@@ -1,24 +1,25 @@
 { config, pkgs, ... }:
 {
-  imports = [./hardware-configuration.nix];
+  imports = [
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+  ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "fowler";
   networking.networkmanager.enable = true;
   time.timeZone = "Europe/Copenhagen";
-  i18n = {
-    defaultLocale = "en_DK.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "da_DK.UTF-8";
-      LC_IDENTIFICATION = "da_DK.UTF-8";
-      LC_MEASUREMENT = "da_DK.UTF-8";
-      LC_MONETARY = "da_DK.UTF-8";
-      LC_NAME = "da_DK.UTF-8";
-      LC_NUMERIC = "da_DK.UTF-8";
-      LC_PAPER = "da_DK.UTF-8";
-      LC_TELEPHONE = "da_DK.UTF-8";
-      LC_TIME = "da_DK.UTF-8";
-    };
+  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "da_DK.UTF-8";
+    LC_IDENTIFICATION = "da_DK.UTF-8";
+    LC_MEASUREMENT = "da_DK.UTF-8";
+    LC_MONETARY = "da_DK.UTF-8";
+    LC_NAME = "da_DK.UTF-8";
+    LC_NUMERIC = "da_DK.UTF-8";
+    LC_PAPER = "da_DK.UTF-8";
+    LC_TELEPHONE = "da_DK.UTF-8";
+    LC_TIME = "da_DK.UTF-8";
   };
   powerManagement = {
     cpuFreqGovernor = "conservative";
@@ -30,49 +31,51 @@
     '';
   };
   services.xserver = {
-    enable = false;
     layout = "us";
-    xkbVariant = "";
+    xkbVariant = "mac";
     xkbOptions = "ctrl:nocaps";
-    # displayManager = {
-    #   startx.enable = true;
-    # };
   };
   console = {
     useXkbConfig = true;
-    font = "ter-i32b";
+    #font = "ter-i32b";
     packages = with pkgs; [ terminus_font ];
     earlySetup = true;
   };
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     helix
-    zsh
-    oh-my-zsh
+    git
+    fzf
     bat
     tree
-    fzf
     direnv
     wget
-    git
-    awscli2
+    zsh
+    oh-my-zsh
   ];
   programs.zsh = {
     enable = true;
     ohMyZsh.enable = true;
   };
+  system.stateVersion = "23.05"; # Do not change
+  security.sudo.wheelNeedsPassword = false;
   users.users.jps = {
     isNormalUser = true;
     description = "Jens Peter Secher";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
-    packages = with pkgs; [
-      dmenu
-      alacritty
-      neofetch
-    ];
   };
-
-  # services.openssh.enable = true;
-  system.stateVersion = "23.05"; # Do not change
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.jps = { pkgs, ... }: {
+    home = {
+      packages = with pkgs; [
+        neofetch
+        awscli2
+        glances
+      ];
+      stateVersion = "23.05";
+      #home-manager.enable = true;
+    };
+  };
 }
