1677e9cc6c008c15192012956d0d8d9173d4291f
Jens Peter Secher <jpsecher@gmail.com>
2023-12-02 19:12:30 +0100

    Enable casting from Brave by default

--- nixos/users/jps/brave.nix
+++ nixos/users/jps/brave.nix
@@ -6,6 +6,7 @@
         # https://peter.sh/experiments/chromium-command-line-switches/
         commandLineArgs = ''
         --load-media-router-component-extension=1
+        --enable-media-router
         --enable-smooth-scrolling
         '';
       };
