e30a19c530621b0e99e70130d74999a839c43073
Jens Peter Secher <jpsecher@gmail.com>
2023-12-03 10:22:51 +0100

    Use OSkey as modifier key

--- common/xmonad/xmonad.hs
+++ common/xmonad/xmonad.hs
@@ -6,9 +6,10 @@ main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
 
 myConfig = def
   { terminal = "alacritty"
+  , modMask = mod4Mask -- Use Super as Modifier key
   }
 
 myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "[" "]" }
 
--- Toggle visibility of xmobar with A-k
+-- Toggle visibility of xmobar with M-b
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
