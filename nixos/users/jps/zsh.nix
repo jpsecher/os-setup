5ccec41f4332315bf7f191b1fe530a8321edf987
Jens Peter Secher <jpsecher@gmail.com>
2023-12-10 20:52:16 +0100

    Drop oh-my-zsh and just use powerlevel10k

--- nixos/users/jps/zsh.nix
+++ nixos/users/jps/zsh.nix
@@ -1,16 +1,29 @@
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     enableSyntaxHighlighting = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = ["git" "docker" "git-extras"];
+    # oh-my-zsh = {
+      # enable = true;
+      # plugins = ["git" "docker" "git-extras" "aws"];
       # plugins = [git docker git-extras ansible pip python ssh-agent];
-      theme = "fino-time";
+      # custom = "$HOME/repos/personal/os-setup/common/zsh";
+      # theme = "jpsecher";
+      # theme = "fino-time";
       #theme = "robbyrussell";
       #theme = "agnoster";
-    };
+    # };
+    plugins = [ {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      } {
+        name = "powerlevel10k-config";
+        src = ../../../common/zsh;
+        file = "p10k.zsh";
+      }
+    ];
     initExtra = "";
     loginExtra = ''
       google-drive-ocamlfuse -label personal ~/gdrive-personal
