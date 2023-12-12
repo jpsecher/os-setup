0e4d0921aa68883594418c92f7cf7e29b9d88b01
Jens Peter Secher <jpsecher@gmail.com>
2023-12-12 20:09:00 +0100

    Switch between full color and TTY theme

--- nixos/users/jps/zsh.nix
+++ nixos/users/jps/zsh.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }:
+let
+  configThemeNormal = ../../../common/zsh/p10k.zsh;
+  configThemeTty = ../../../common/zsh/p10k-portable.zsh;
+in
 {
   programs.zsh = {
     enable = true;
@@ -8,18 +12,21 @@
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      } {
-        name = "powerlevel10k-config";
-        src = ../../../common/zsh;
-        file = "p10k.zsh";
       }
+      # {
+      #   name = "powerlevel10k-config";
+      #   src = ../../../common/zsh;
+      #   file = "p10k.zsh";
+      # }
     ];
-    # if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
-    #   [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-    # else
-    #   [[ ! -f ~/.p10k-portable.zsh ]] || source ~/.p10k-portable.zsh
-    # fi
-    initExtra = "";
+    initExtra = ''
+      # See https://github.com/NixOS/nixpkgs/issues/154696#issuecomment-1238433989
+      if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
+        [[ ! -f ${configThemeNormal} ]] || source ${configThemeNormal}
+      else
+        [[ ! -f ${configThemeTty} ]] || source ${configThemeTty}
+      fi
+    '';
     loginExtra = ''
       google-drive-ocamlfuse -label personal ~/gdrive-personal
     '';
