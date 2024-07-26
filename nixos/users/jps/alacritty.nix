00777574abec906834758a5ac67c285e612c2beb
Jens Peter Secher <jpsecher@gmail.com>
2024-07-26 05:20:15 +0200

    macbook air mermin

--- nixos/users/jps/alacritty.nix
+++ nixos/users/jps/alacritty.nix
@@ -1,4 +1,4 @@
 {
   programs.alacritty.enable = true;
-  xdg.configFile."alacritty/alacritty.toml".source = ../../../common/alacritty/alacritty.toml;
+  xdg.configFile."alacritty/alacritty.toml".source = ../../../common/alacritty/alacritty-small.toml;
 }
