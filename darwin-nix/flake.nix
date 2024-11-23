029967469046dc448abfb90316efa1aab17dcf1c
Jens Peter Secher <jpsecher@gmail.com>
2024-11-23 15:00:29 +0100

    Spotlight settings

--- darwin-nix/flake.nix
+++ darwin-nix/flake.nix
@@ -55,30 +55,39 @@
         screencapture.location = "~/Downloads";
         screensaver.askForPasswordDelay = 10;
         LaunchServices.LSQuarantine = false;
-        # CustomUserPreferences."com.apple.Spotlight"."orderedItems" = [
-        #   { enabled = 1; name = "APPLICATIONS"; }
-        #   { enabled = 1; name = "MENU_EXPRESSION"; }
-        #   { enabled = 0; name = "CONTACT"; }
-        #   { enabled = 1; name = "MENU_CONVERSION"; }
-        #   { enabled = 0; name = "MENU_DEFINITION"; }
-        #   { enabled = 0; name = "SOURCE"; }
-        #   { enabled = 0; name = "DOCUMENTS"; }
-        #   { enabled = 0; name = "EVENT_TODO"; }
-        #   { enabled = 0; name = "DIRECTORIES"; }
-        #   { enabled = 0; name = "FONTS"; }
-        #   { enabled = 0; name = "IMAGES"; }
-        #   { enabled = 0; name = "MESSAGES"; }
-        #   { enabled = 0; name = "MOVIES"; }
-        #   { enabled = 0; name = "MUSIC"; }
-        #   { enabled = 0; name = "MENU_OTHER"; }
-        #   { enabled = 0; name = "PDF"; }
-        #   { enabled = 0; name = "PRESENTATIONS"; }
-        #   { enabled = 0; name = "MENU_SPOTLIGHT_SUGGESTIONS"; }
-        #   { enabled = 0; name = "SPREADSHEETS"; }
-        #   { enabled = 1; name = "SYSTEM_PREFS"; }
-        #   { enabled = 0; name = "TIPS"; }
-        #   { enabled = 0; name = "BOOKMARKS"; }
-        # ];
+        NSGlobalDomain = {
+          NSAutomaticSpellingCorrectionEnabled = false;
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+        };
+        CustomUserPreferences."com.apple.Spotlight"."orderedItems" = [
+          { enabled = 1; name = "APPLICATIONS"; }
+          { enabled = 1; name = "MENU_EXPRESSION"; }
+          # { enabled = 0; name = "CALCULATOR"; }  # breaks the window
+          # { enabled = 0; name = "CONTACT"; }  # breaks the window
+          # { enabled = 1; name = "MENU_CALCULATOR"; }  # breaks the window
+          { enabled = 0; name = "MENU_CONVERSION"; }
+          { enabled = 0; name = "MENU_DEFINITION"; }
+          { enabled = 0; name = "SOURCE"; }
+          { enabled = 0; name = "DOCUMENTS"; }
+          { enabled = 0; name = "EVENT_TODO"; }
+          { enabled = 1; name = "DIRECTORIES"; }
+          { enabled = 0; name = "FONTS"; }
+          { enabled = 0; name = "IMAGES"; }
+          { enabled = 0; name = "MESSAGES"; }
+          { enabled = 0; name = "MOVIES"; }
+          { enabled = 0; name = "MUSIC"; }
+          { enabled = 0; name = "MENU_OTHER"; }
+          { enabled = 0; name = "PDF"; }
+          { enabled = 0; name = "PRESENTATIONS"; }
+          { enabled = 0; name = "MENU_SPOTLIGHT_SUGGESTIONS"; }
+          { enabled = 0; name = "SPREADSHEETS"; }
+          { enabled = 1; name = "SYSTEM_PREFS"; }
+          { enabled = 0; name = "TIPS"; }
+          { enabled = 0; name = "BOOKMARKS"; }
+        ];
       };
       # system.activationScripts.applications.text = ''
       #   echo "setting up ~/Applications/Nix..."
