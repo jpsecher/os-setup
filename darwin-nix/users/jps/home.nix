ee264a3e72e10666aecfb4aab7070ae4cb85ea6a
Jens Peter Secher <jpsecher@gmail.com>
2024-12-23 23:14:56 +0100

    still problems with Spotlight

--- darwin-nix/users/jps/home.nix
+++ darwin-nix/users/jps/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   imports = [
     ./alacritty.nix
     ./git.nix
@@ -60,7 +60,7 @@
     rust-analyzer
     # rustup
     # sd  # String replacement
-    # signal-desktop
+    signal-desktop
     slack
     sqlite
     ssm-session-manager-plugin  # AWS SSM CLI
@@ -72,4 +72,16 @@
     tree
     watch
   ];
+  # Has to be uncommented because Signal gives problems:
+  # https://github.com/LnL7/nix-darwin/issues/214
+  # home.activation = {
+  #   rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #     apps_source="$genProfilePath/home-path/Applications"
+  #     moniker="Nix Trampolines"
+  #     app_target_base="$HOME/Applications"
+  #     app_target="$app_target_base/$moniker"
+  #     mkdir -p "$app_target"
+  #     ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+  #   '';
+  # };
 }
