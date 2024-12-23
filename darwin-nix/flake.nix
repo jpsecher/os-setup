8664492617a3e617aa561fd385bb5aed1a55f555
Jens Peter Secher <jpsecher@gmail.com>
2024-12-23 21:29:07 +0100

    wip: switching to 24.11

--- darwin-nix/flake.nix
+++ darwin-nix/flake.nix
@@ -2,10 +2,10 @@
   description = "JP's nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     ## https://github.com/NixOS/nixpkgs/issues/355377
     ghostscript-fix.url = "github:carlocab/nixpkgs/fix-ghostscript";
@@ -104,14 +104,14 @@
         ];
       };
       # https://github.com/LnL7/nix-darwin/issues/214
-      system.activationScripts.postUserActivation.text = ''
-        apps_source="$HOME/Applications/Home Manager Apps"
-        moniker="Nix Trampolines"
-        app_target_base="$HOME/Applications"
-        app_target="$app_target_base/$moniker"
-        mkdir -p "$app_target"
-        ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
-      '';
+      # system.activationScripts.postUserActivation.text = ''
+      #   apps_source="$HOME/Applications/Home Manager Apps"
+      #   moniker="Nix Trampolines"
+      #   app_target_base="$HOME/Applications"
+      #   app_target="$app_target_base/$moniker"
+      #   mkdir -p "$app_target"
+      #   ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+      # '';
 
       nix.extraOptions = ''
         extra-platforms = x86_64-darwin aarch64-darwin
