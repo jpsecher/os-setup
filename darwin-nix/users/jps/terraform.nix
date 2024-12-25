fe18fbfcbce3ddefd5828e740ac005b0a809c602
Jens Peter Secher <jpsecher@gmail.com>
2024-12-25 22:29:21 +0100

    comments

--- darwin-nix/users/jps/terraform.nix
+++ darwin-nix/users/jps/terraform.nix
@@ -1,13 +1,17 @@
-# nix-env -iA terraform -f https://github.com/NixOS/nixpkgs/archive/3f293ea9ecd5c50e5bd393fd1c560275ea0e6975.tar.gz
+## Right now, the old version of terraform is installed by
+## nix-env -iA terraform -f https://github.com/NixOS/nixpkgs/archive/3f293ea9ecd5c50e5bd393fd1c560275ea0e6975.tar.gz
+## but I would like to do it like this:
+{ pkgs, ... }:
 let
-  pkgs = import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
+  oldpkgs = import (builtins.fetchGit {
     name = "v1.5.7";
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/heads/nixpkgs-24.05-darwin";
     rev = "3f293ea9ecd5c50e5bd393fd1c560275ea0e6975";
-  }) {};
-  terraform_1_5_7 = pkgs.terraform;
+  }) {
+    inherit (pkgs) system;
+  };
+  terraform_1_5_7 = oldpkgs.terraform;
 in
 {
   programs.terraform = {
