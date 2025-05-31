45574e6d9f930a6755d5eda08a92a96b8b38a7e5
Jens Peter Secher <jpsecher@gmail.com>
2025-05-31 12:11:49 +0200

    Still need Trampolines for Spotlight

--- darwin-nix/users/jps/home.nix
+++ darwin-nix/users/jps/home.nix
@@ -92,4 +92,15 @@
     # kicad
     # signal-desktop  # Messaging
   ];
+  # https://github.com/LnL7/nix-darwin/issues/214
+  home.activation = {
+    rsync-home-manager-applications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      apps_source="$genProfilePath/home-path/Applications"
+      moniker="Nix Trampolines"
+      app_target_base="$HOME/Applications"
+      app_target="$app_target_base/$moniker"
+      mkdir -p "$app_target"
+      ${pkgs.rsync}/bin/rsync --archive --checksum --chmod=-w --copy-unsafe-links --delete "$apps_source/" "$app_target"
+    '';
+  };
 }
